9b6e87bcdea3841106f8e646b93402ccb42b31e2
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-01-05 15:20:56 -0400

    Metodos con argumentos

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,7 +1,32 @@
 public class Automovil {
     String fabricante ;
-    String marca;
+    String marca= "Prueba";
     String modelo;
     double cilindrada;
-    
+
+    public String verDetalle(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("fabricante = ").append(this.fabricante);
+        sb.append("\nmarca = ").append(this.marca);
+        sb.append("\nmodelo = ").append(this.modelo);
+        sb.append("\ncilindrada = ").append(this.cilindrada);
+        return sb.toString();
+    }
+
+    public String realizarAceleleracion(int rpm){
+        return  "El automovil acelera a" + rpm +"    Fabricante   " + this.fabricante;
+    }
+     public String realizarFrenado(){
+        return "El automovil esta frenando" + this.modelo + "   Santy  ";
+    }
+
+    public String acelerarFrenar(int rpm){
+       String reazlizarAceleracion = this.realizarAceleleracion(rpm);
+       String realizarFrenado = this.realizarFrenado();
+       return  reazlizarAceleracion + "\n" + realizarFrenado;
+    }
+
+
+
+
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,26 +1,35 @@
 public class EjemploAutomovil {
 
     public static void main (String[] args){
-        Automovil automovil = new Automovil();
+        Automovil automovilFord = new Automovil();
 
 
-        // Asignacion de valores
-        automovil.fabricante = "Ford";
-        automovil.marca = "Fiesta";
-        automovil.modelo = "Sedan";
-        automovil.cilindrada = 1.6;
+        // Asignacion de valores Subaru
+        automovilFord.fabricante = "Ford";
+        automovilFord.marca = "Fiesta";
+        automovilFord.modelo = "Sedan";
+        automovilFord.cilindrada = 1.6;
 
+        // Asignacion de valores Mazda
         Automovil automovilMazda = new Automovil();
         automovilMazda.fabricante = "Mazda";
         automovilMazda.marca = "Mazda 3";
         automovilMazda.modelo = "Sedan";
         automovilMazda.cilindrada = 2.0;
 
+
+        System.out.println(automovilMazda.verDetalle());
+        System.out.println(automovilFord.verDetalle());
+
+        System.out.println(automovilMazda.acelerarFrenar(3000));
+
+
+/*
         System.out.println("Instancia Automovil generic"  );
-        System.out.println("automovil.fabricante = " + automovil.fabricante);
-        System.out.println("automovil.marca = " + automovil.marca);
-        System.out.println("automovil.modelo = " + automovil.modelo);
-        System.out.println("automovil.cilindrada = " + automovil.cilindrada);
+        System.out.println("automovilFord.fabricante = " + automovilFord.fabricante);
+        System.out.println("automovilFord.marca = " + automovilFord.marca);
+        System.out.println("automovilFord.modelo = " + automovilFord.modelo);
+        System.out.println("automovilFord.cilindrada = " + automovilFord.cilindrada);
 
 
         System.out.println("Instancia Mazda"  );
@@ -28,6 +37,7 @@ public class EjemploAutomovil {
         System.out.println("automovilMazda.marca = " + automovilMazda.marca);
         System.out.println("automovilMazda.modelo = " + automovilMazda.modelo);
         System.out.println("automovilMazda.cilindrada = " + automovilMazda.cilindrada);
+  */
     }
 
 
